433b4cbfb8b1b0507fc4afa15a5636ce19541dc2
vugomars <vugomars@gmail.com>
2022-09-20 01:24:57 +0700

    Doom Configuration Files + introduction

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is an automatically generated setup file, it installes some missing
+# dependencies, configure system services, set system settings form better
+# desktop integration... etc.
+# Dang Quang Vu <vugomars@gmail.com> (c) 2022
