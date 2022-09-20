17071aac0fa6a7dbd150f9aa1628a0f3c51995cc
vugomars <vugomars@gmail.com>
2022-09-20 08:45:30 +0700

    Treemac setting

--- config.el
+++ config.el
@@ -522,3 +522,67 @@
   ;; Reload icons even if not in Daemon mode
   (+company-box--reload-icons-h))
 ;; Tweak =company-box=:1 ends here
+
+;; [[file:config.org::*Treemacs][Treemacs:1]]
+(after! treemacs
+  (require 'dired)
+
+  ;; My custom stuff (from tecosaur's config)
+  (setq +treemacs-file-ignore-extensions
+        '(;; LaTeX
+          "aux" "ptc" "fdb_latexmk" "fls" "synctex.gz" "toc"
+          ;; LaTeX - bibliography
+          "bbl"
+          ;; LaTeX - glossary
+          "glg" "glo" "gls" "glsdefs" "ist" "acn" "acr" "alg"
+          ;; LaTeX - pgfplots
+          "mw"
+          ;; LaTeX - pdfx
+          "pdfa.xmpi"
+          ;; Python
+          "pyc"))
+
+  (setq +treemacs-file-ignore-globs
+        '(;; LaTeX
+          "*/_minted-*"
+          ;; AucTeX
+          "*/.auctex-auto"
+          "*/_region_.log"
+          "*/_region_.tex"
+          ;; Python
+          "*/__pycache__"))
+
+  ;; Reload treemacs theme
+  (setq doom-themes-treemacs-enable-variable-pitch nil
+        doom-themes-treemacs-theme "doom-colors")
+  (doom-themes-treemacs-config)
+
+  (setq treemacs-show-hidden-files nil
+        treemacs-hide-dot-git-directory t
+        treemacs-width 30)
+
+  (defvar +treemacs-file-ignore-extensions '()
+    "File extension which `treemacs-ignore-filter' will ensure are ignored")
+
+  (defvar +treemacs-file-ignore-globs '()
+    "Globs which will are transformed to `+treemacs-file-ignore-regexps' which `+treemacs-ignore-filter' will ensure are ignored")
+
+  (defvar +treemacs-file-ignore-regexps '()
+    "RegExps to be tested to ignore files, generated from `+treeemacs-file-ignore-globs'")
+
+  (defun +treemacs-file-ignore-generate-regexps ()
+    "Generate `+treemacs-file-ignore-regexps' from `+treemacs-file-ignore-globs'"
+    (setq +treemacs-file-ignore-regexps (mapcar 'dired-glob-regexp +treemacs-file-ignore-globs)))
+
+  (unless (equal +treemacs-file-ignore-globs '())
+    (+treemacs-file-ignore-generate-regexps))
+
+  (defun +treemacs-ignore-filter (file full-path)
+    "Ignore files specified by `+treemacs-file-ignore-extensions', and `+treemacs-file-ignore-regexps'"
+    (or (member (file-name-extension file) +treemacs-file-ignore-extensions)
+        (let ((ignore-file nil))
+          (dolist (regexp +treemacs-file-ignore-regexps ignore-file)
+            (setq ignore-file (or ignore-file (if (string-match-p regexp full-path) t nil)))))))
+
+  (add-to-list 'treemacs-ignored-file-predicates #'+treemacs-ignore-filter))
+;; Treemacs:1 ends here
