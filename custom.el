3bf537452f3bf6b4e17040e78596c7d14dbb57f0
vugomars <vugomars@gmail.com>
2022-09-20 09:35:49 +0700

    Enabled for unicode & emoji

--- custom.el
+++ custom.el
@@ -3,10 +3,36 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(modus-themes lsp-ltex)))
+ '(package-selected-packages
+   '(ascii-table unicode-fonts emojify unidecode modus-themes lsp-ltex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-hl-change ((((class color) (min-colors 89)) :foreground "#dbba3f")))
+ '(diff-hl-delete ((((class color) (min-colors 89)) :foreground "#f08290")))
+ '(diff-hl-insert ((((class color) (min-colors 89)) :foreground "#62c86a")))
+ '(doom-modeline-evil-insert-state ((((class color) (min-colors 89)) :foreground "#a0132f")))
+ '(doom-modeline-evil-motion-state ((((class color) (min-colors 89)) :foreground "#0000c0")))
+ '(doom-modeline-evil-normal-state ((((class color) (min-colors 89)) :foreground "#145c33")))
+ '(doom-modeline-evil-operator-state ((((class color) (min-colors 89)) :foreground "#2544bb")))
+ '(doom-modeline-evil-replace-state ((((class color) (min-colors 89)) :foreground "#70480f")))
+ '(doom-modeline-evil-visual-state ((((class color) (min-colors 89)) :foreground "#8f0075")))
+ '(evil-mc-cursor-default-face ((((class color) (min-colors 89)) :background "#d5baff")))
+ '(fill-column-indicator ((((class color) (min-colors 89)) :height 0.3 :background "#efefef" :foreground "#efefef")))
+ '(git-gutter-fr:added ((((class color) (min-colors 89)) :foreground "#62c86a")))
+ '(git-gutter-fr:deleted ((((class color) (min-colors 89)) :foreground "#f08290")))
+ '(git-gutter-fr:modified ((((class color) (min-colors 89)) :foreground "#dbba3f")))
+ '(mmm-cleanup-submode-face ((((class color) (min-colors 89)) :background "#fff29a")))
+ '(mmm-code-submode-face ((((class color) (min-colors 89)) :background "#d7d7d7")))
+ '(mmm-comment-submode-face ((((class color) (min-colors 89)) :background "#8fcfff")))
+ '(mmm-declaration-submode-face ((((class color) (min-colors 89)) :background "#8eecf4")))
+ '(mmm-default-submode-face ((((class color) (min-colors 89)) :background "#f0f0f0")))
+ '(mmm-init-submode-face ((((class color) (min-colors 89)) :background "#ffccff")))
+ '(mmm-output-submode-face ((((class color) (min-colors 89)) :background "#ffcccc")))
+ '(mmm-special-submode-face ((((class color) (min-colors 89)) :background "#aceaac")))
+ '(solaire-default-face ((((class color) (min-colors 89)) :inherit default :background "#f0f0f0" :foreground "#282828")))
+ '(solaire-hl-line-face ((((class color) (min-colors 89)) :background "#d7d7d7")))
+ '(solaire-line-number-face ((((class color) (min-colors 89)) :inherit solaire-default-face :foreground "#56576d")))
+ '(solaire-org-hide-face ((((class color) (min-colors 89)) :background "#f0f0f0" :foreground "#f0f0f0"))))
--- init.el
+++ init.el
@@ -36,7 +36,7 @@
        (popup +all +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        (treemacs +lsp)          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        (vc-gutter +diff-hl +pretty)         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select +numbers)     ; visually switch windows
